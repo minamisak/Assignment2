e2d86f4e7a86bc786017a72b377f05b0f9234c13
Kareem Ashraf <k.ashraf.salah@gmail.com>
2021-07-14 10:31:38 +0200

    greeting added

--- kareem.py
+++ kareem.py
@@ -1,10 +1,14 @@
 print("hello kareem")
-
+print("hello Zima")
+print("hello Joyti")
+print("hello Hedra")
+print("hello Himansho")
 
 #### greating from Zima
 
 #### greating from Joyti
 
 #### greating from Hedra
+
 
 #### greating from Himansho
